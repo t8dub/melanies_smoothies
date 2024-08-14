ab43a1f46afee3612b2f924a2eedfea1edf62f46
t8dub <153192132+t8dub@users.noreply.github.com>
2024-08-14 07:15:12 -0700

    Update streamlit_app.py
    Changing title and text to menu options

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,12 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.title("My Parent's New Healthy Diner")
 st.write(
-    """Choose the fruits you want in your custom Smoothie!
+    """Breakfast Menu
+    Omega 3 & Blueberry Oatmeal
+    Kale, Spinach & Rocket Smoothie
+    Hard-Boiled Free-Range Egg        
     """
 )
 
